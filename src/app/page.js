563d26d24c2e40490769331d5b15a1460a8ff536
Abhishek <abhishekdaszy@gmail.com>
2024-01-04 02:15:08 +0530

    media queries for mobiles

--- src/app/page.js
+++ src/app/page.js
@@ -108,10 +108,14 @@ const Page = () => {
           <p> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
         </div>
         <div className="instructor-socialMedia">
-          <div className="socialMedia"> <img src="/facebook.png" alt="facebook" /> <p> Facebook </p> </div>
-          <div className="socialMedia"> <img src="/twitterX.png" alt="twitterX" /> <p> Twitter </p> </div>
-          <div className="socialMedia"> <img src="/youtube.png" alt="youtube" /> <p> Youtube </p> </div>
-          <div className="socialMedia"> <img src="/instagram.png" alt="instagram" /> <p> Instagram </p> </div>
+          <div id="social">
+            <div className="socialMedia"> <img src="/facebook.png" alt="facebook" /> <p> Facebook </p> </div>
+            <div className="socialMedia"> <img src="/twitterX.png" alt="twitterX" /> <p> Twitter </p> </div>
+          </div>
+          <div id="social">
+            <div className="socialMedia"> <img src="/youtube.png" alt="youtube" /> <p> Youtube </p> </div>
+            <div className="socialMedia"> <img src="/instagram.png" alt="instagram" /> <p> Instagram </p> </div>
+          </div>
         </div>
       </div>
 
