b0e8c1ff6a7d34dea35a6321252ada1bb0b5aa88
Abhishek <abhishekdaszy@gmail.com>
2024-01-03 19:10:16 +0530

    created the registrationCard

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,83 @@
-import React from 'react';
-import './global.scss';
+import React from "react";
+import "./global.scss";
+import { constants } from "./constants";
 
 const Page = () => {
+  const { course } = constants;
   return (
     <div>
       <div className="hero-sec">
-        
         {/* course-details */}
-        <div className="course-headings" style={{ backgroundImage: "url('./authorImg.png')" }}>
+        <div
+          className="course-headings"
+          style={{ backgroundImage: "url('./authorImg.png')" }}
+        >
           <div className="course-det">
             <p className="auth-name"> NITYANAND CHARAN DAS </p>
-            <h1 className="course-name"> Learn key life lessons from the Gita: Mind control and conflict resolution </h1>
-          </div>
-        </div>
-        
-        
-        <div className="nav">
-          <div className="navbar">
-            <p id='active'> ABOUT </p>
-            <p> INSTRUCTOR </p>
-            <p> REVIEWS </p>
+            <h1 className="course-name"> Learn key life lessons from the Gita: Mind control and conflictresolution </h1>
           </div>
         </div>
 
-        <div className="aboutCourse">
-          <h1> About the course </h1>
-          <p> Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
-          <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-          <p> Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. </p>
+        <div className="course-details">
+          <div className="courseAndabout">
+            <div className="nav">
+              <div className="navbar">
+                <p id="active"> ABOUT </p>
+                <p> INSTRUCTOR </p>
+                <p> REVIEWS </p>
+              </div>
+            </div>
+
+            <div className="aboutCourse">
+              <h2> About the course </h2>
+              <p>
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                consequatur? Quis autem vel eum iure reprehenderit qui in ea
+                voluptate velit esse quam nihil molestiae consequatur, vel illum
+                qui dolorem eum fugiat quo voluptas nulla pariatur?
+              </p>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo.
+              </p>
+              <p>
+                Temporibus autem quibusdam et aut officiis debitis aut rerum
+                necessitatibus saepe eveniet ut et voluptates repudiandae sint
+                et molestiae non recusandae. Itaque earum rerum hic tenetur a
+                sapiente delectus, ut aut reiciendis voluptatibus maiores alias
+                consequatur aut perferendis doloribus asperiores repellat.
+              </p>
+            </div>
+
+            <div className="course-expectations">
+              <h2> What to expect from the course </h2>
+              <ul className="expectations">
+                {course.what_to_expect.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="registrationCard">
+            <div className="fees">
+              <h4> Course fees </h4>
+              <h1> ₹5,000 </h1>
+            </div>
+            <div className="features">
+              <h4> What's Included: </h4>
+              <p> 5 on-demand videos </p>
+              <p> 2 livestream sessions </p>
+              <p> Live Q&A sessions with Nityananda Charan Das </p>
+              <p> An active whatsapp community </p>
+            </div>
+
+            <div className="btn"> Register today </div>
+          </div>
         </div>
-      
       </div>
     </div>
   );
